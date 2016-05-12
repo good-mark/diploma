515178e5e3fb2166e68b498eaf15d587292aad2d
Воробьева Елена Юрьевна <vorobeva@phystech.edu>
2016-05-12 20:39:41 +0300

    feature selection added

--- malt_parser.py
+++ malt_parser.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.sparse import csr_matrix 
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 import pickle
 from collections import defaultdict
@@ -21,11 +22,13 @@ import array
 import csv
 import os
 
+import time
+
 # ===================================================
 # Constant declaration
 # ===================================================
-dataset_path = '..\\test_building_train_data_small_15'
-#dataset_path = '..\\fix_test'
+#dataset_path = '.\\test_building_train_data_small_15'
+dataset_path = '.\\fix_test'
 SENTENCE_ID = 0
 LEX = 3
 ID = 4
@@ -107,19 +110,20 @@ class DataKeeper:
 						# filtering the sentences with no root vertex (is it possible? seems to be)
 						if row[PARENT] == '-1':
 							is_cur_sent_content_root = True
-						if len(current_sentence) >= 5 and is_cur_sent_content_root:
-							# add fictive root vertex in the end of sentence.
-							# Root's parent is ROOT!!!
-							current_sentence.append(row)
+						if len(current_sentence) >= 5:
+							if is_cur_sent_content_root:
+								# add fictive root vertex in the end of sentence.
+								# Root's parent is ROOT!!!
+								current_sentence.append(row)
 
-							root = self.get_root(prev_sentence_num)
-							current_sentence.append(list(root))
-							current_sentence_features.append(root[6:])
+								root = self.get_root(prev_sentence_num)
+								current_sentence.append(list(root))
+								current_sentence_features.append(root[6:])
 
-							self.input.append(list(current_sentence))
-							self.features.extend(list(current_sentence_features))
-							break
-						print "Sentence", idx, "has not root vertex!"
+								self.input.append(list(current_sentence))
+								self.features.extend(list(current_sentence_features))
+								break
+							print "Sentence", idx, "has not root vertex"
 						break
 
 					# if token is not last one in input,
@@ -179,6 +183,7 @@ class MaltParser:
 		self.data_keeper.fill_dictionary()
 
 		self.svm_clf = OneVsRestClassifier(SVC(kernel='linear', verbose=True), n_jobs=-1)
+		self.rndforest = RandomForestClassifier()
 		self.tokens = []
 		self.stack = [] # stack for deterministic algorithm
 		self.input = self.data_keeper.input
@@ -192,6 +197,8 @@ class MaltParser:
 		# for training, filled in build_train_samples
 		self.train_samples = []
 		self.train_answers = []
+
+		self.important_features = [] # feature selection
 
 		self.row = []
 		self.col = []
@@ -226,11 +233,15 @@ class MaltParser:
 	# filling the history feature-based model.
 	# ===================================================
 	def add_one_feature(self, feature, current_row, order):
-		feat_num = self.data_keeper.get_feat_num_by_name[feature]
+		feat_num = self.data_keeper.get_feat_num_by_name(feature)
 		self.row.append(current_row)
 		self.col.append(feat_num + self.feat_count * order)
 		self.data.append(1)
 
+	# Feat_num is a flag if we want use smart feature map constructor or usual.
+	# Smart constructor selects only top 500 important features, after feature selection.
+	# In this case, feat_num is an index of selected feature.
+	# Usual one collect all the features. feat_num == -1.
 	def add_top_features(self, order, current_row):
 		if (len(self.stack) != 0):
 			for feature in self.stack[-1][FEATURES:]:
@@ -343,71 +354,81 @@ class MaltParser:
 		rrow = np.asarray(self.row)
 		rcol = np.asarray(self.col)
 		self.train_samples = csr_matrix( ( rdata,(rrow,rcol) ) )
+		self.filter_features() # select only top 500 the most important
 		#self.train_samples = sparse.CSR(rdata, rrow, rcol, (len(self.train_answers), self.feat_count * 10))
 		#rdata, rrow, rcol = sparse.csm_properties(self.train_samples)
 
 	def build_train_samples(self, begin, end):
 		print 'building train samples...'
-		self.erase_containers() # init containers
+		if len(self.train_answers) != 0:
+			print 'Train samples are already ready!'
+			print self.train_samples.shape[0]
+			end = self.train_samples.shape[0] * end / len(self.input)
+			self.train_samples = self.train_samples[begin:end]
+			self.filter_features()
+			self.train_answers = self.train_answers[begin:end]
+		else:
+			self.erase_containers() # init containers
 
-		# will use first half of data in range from begin index to end index
-		print 'size of training data is ', end - begin, 'sentences'
+			# will use first half of data in range from begin index to end index
+			print 'size of training data is ', end - begin, 'sentences'
 
-		self.current_sentence_position = begin
-		for sentence in self.input[begin:end]:
-			self.erase_state()
-			#print self.stack #DEBUG
-			if self.current_sentence_position % 5000 == 0:
-				print self.current_sentence_position
+			self.current_sentence_position = begin
+			for sentence in self.input[begin:end]:
+				self.erase_state()
+				#print self.stack #DEBUG
+				if self.current_sentence_position % 5000 == 0:
+					print self.current_sentence_position
 
-			stop_idx = len(sentence)
-			# do transitions inside one sentence
-			# remember the last vertex is ROOT
-			for idx, word_with_features in enumerate(sentence):
-				self.current_word_position = idx
-				# until right or shift
-				# priority is left > right > reduce > shift
-				while self.current_word_position < stop_idx:
-					# create history-based feature model instance
-					# and add it to train data
-					self.add_history_feature_map_for_training()
-					if len(self.stack) == 0:
-						self.train_answers.append(SHIFT)
-						self.shift()						
-						break
-					if self.stack[-1][PARENT] == word_with_features[ID]:
-						self.train_answers.append(LEFTARC)
-						self.left_arc()
-						continue
-					if self.stack[-1][ID] == word_with_features[PARENT]:
-						self.train_answers.append(RIGHTARC)
-						self.right_arc()
-						break
-					for stack_word in self.stack:
-						has_stack_dependencies = False
-						if word_with_features[ID] == stack_word[PARENT]:
-							has_stack_dependencies = True
+				stop_idx = len(sentence)
+				# do transitions inside one sentence
+				# remember the last vertex is ROOT
+				for idx, word_with_features in enumerate(sentence):
+					self.current_word_position = idx
+					# until right or shift
+					# priority is left > right > reduce > shift
+					while self.current_word_position < stop_idx:
+						# create history-based feature model instance
+						# and add it to train data
+						self.add_history_feature_map_for_training()
+						if len(self.stack) == 0:
+							self.train_answers.append(SHIFT)
+							self.shift()
 							break
-					if has_stack_dependencies:
-						self.train_answers.append(REDUCE)
-						self.reduce()
-						continue
-					for right_word in sentence[idx+1:]:
-						has_right_dependencies = False
-						if right_word[ID] == word_with_features[PARENT] or right_word[PARENT] == word_with_features[ID]:
-							has_right_dependencies = True
+						if self.stack[-1][PARENT] == word_with_features[ID]:
+							self.train_answers.append(LEFTARC)
+							self.left_arc()
+							continue
+						if self.stack[-1][ID] == word_with_features[PARENT]:
+							self.train_answers.append(RIGHTARC)
+							self.right_arc()
 							break
-					if has_right_dependencies:
-						self.train_answers.append(SHIFT)
-						self.shift()
-						break
-					else:
-						self.train_answers.append(REDUCE)
-						self.reduce()
-			self.current_sentence_position += 1
+						for stack_word in self.stack:
+							has_stack_dependencies = False
+							if word_with_features[ID] == stack_word[PARENT]:
+								has_stack_dependencies = True
+								break
+						if has_stack_dependencies:
+							self.train_answers.append(REDUCE)
+							self.reduce()
+							continue
+						for right_word in sentence[idx+1:]:
+							has_right_dependencies = False
+							if right_word[ID] == word_with_features[PARENT] or right_word[PARENT] == word_with_features[ID]:
+								has_right_dependencies = True
+								break
+						if has_right_dependencies:
+							self.train_answers.append(SHIFT)
+							self.shift()
+							break
+						else:
+							self.train_answers.append(REDUCE)
+							self.reduce()
+				self.current_sentence_position += 1
 
-		self.build_train_sparse_matrix()
-		print 'train samples were built!'
+			self.build_train_sparse_matrix()
+			print 'train samples were built!'
+
 
 	# For each sentence predict consequence of actions,
 	# parse it using this actions and evaluate result
@@ -474,11 +495,42 @@ class MaltParser:
 		print 'general score ', general_score
 		print '!!! accuracy is ', general_score * 1.0 / len(self.input[begin:end])
 
-	# do experiment and count the score
+	def filter_features(self):
+		if len(self.important_features) != 0:
+			dense_train_samples = self.train_samples.toarray()
+			print len(dense_train_samples)
+			self.train_samples = []
+			for raw in dense_train_samples:
+				new_raw = []
+				for feat_num in self.important_features:
+					new_raw.append(raw[feat_num])
+				self.train_samples.append(new_raw)
+
+	def select_features(self):
+		self.build_train_samples(0, len(self.input)-1)
+		print 'start features selection...'
+		print 'start training...'
+		self.rndforest.fit(self.train_samples, self.train_answers)
+		print 'end training...'
+		svc_weights = self.rndforest.feature_importances_
+		print svc_weights[:-1]
+
+		self.important_features = svc_weights.argsort()[-500:][::-1]
+		print self.important_features
+
+		with open('500important_features', "wt") as f:
+			print len(self.important_features)
+			f.write(str(len(self.important_features)) + "\n")
+			for feature in self.important_features:
+				f.write(str(feature) + ' ')
+				num = feature % self.feat_count
+				f.write(str(feature / self.feat_count) + ' ' + str(num) + ' ' + self.data_keeper.features_names[num] + '\n')
+
 	def execute_svm_experiment(self, train_part):
 		print 'Experiment starting...'
 		train_data_size = int(len(self.input) * train_part) # number of _sentences_
 		self.build_train_samples(0, train_data_size)
+
 		print "Number of train samples: ", self.train_samples.shape
 
 		# train model
@@ -503,8 +555,8 @@ class MaltParser:
 	def execute_network_experiment(self, train_part):
 		print 'experiment starting...'
 		train_data_size = int(len(self.input) * train_part) # number of _sentences_
-		mparser.build_train_samples(0, train_data_size)
-		print "number of train samples: ", self.train_samples.shape
+		self.build_train_samples(0, train_data_size)
+		print "number of train samples: ", len(self.train_samples)
 
 		# work with network
 		print("Building network ...")
@@ -516,15 +568,15 @@ class MaltParser:
    		#input_var = theano.sparse.csr_matrix(name='inputs', dtype='int16')
 	 	input_var = T.tensor3('inputs')
 	 	target_var = T.matrix('targets')
-	 	train_matrix = self.train_samples.todense()
-	 	print train_matrix.shape[0], train_matrix.shape[1], 'SHAPEEE'
+	 	train_matrix = self.train_samples
+	 	print len(train_matrix[0]), len(train_matrix), 'SHAPEEE'
 
 
 		# ===================================================
 		# Declarate the network architecture, layers.
 		# ===================================================
 	 	# (batch size, SEQ_LENGTH, num_features)
-	 	l_in = lasagne.layers.InputLayer(shape=(1, 1, train_matrix.shape[1]), input_var=input_var)
+	 	l_in = lasagne.layers.InputLayer(shape=(1, 1, 500), input_var=input_var)
 	 	l_in_drop = lasagne.layers.DropoutLayer(l_in, p=0.3)
 
 		#l_resized = lasagne.layers.ReshapeLayer(l_in_drop, shape=(-1, 1))
@@ -574,19 +626,23 @@ class MaltParser:
 
 		print "Training ..."
 	 	for idx, row in enumerate(train_matrix):
+			#_startTime1 = time.time()
 			if idx % 1000 == 0:
 				print "training", idx
-			inputs = np.array([row])
+			inputs = np.array(row).reshape([1,1,500])
+			#print type(inputs)#, inputs.shape
 			targets = self.construct_targets_for_network(self.train_answers[idx])
+	 		#_startTime2 = time.time()
 	 		avg_cost = train(inputs, targets)
+			#_startTime3 = time.time()
+			#print "sum time", _startTime3 - _startTime1, "reading", _startTime2 - _startTime1
 
 		print "Testing..."
 		# For each sentence predict consequence of actions, parse it using this actions and evaluate result
 		general_score = 0
 		self.current_sentence_position = train_data_size #!!!!!!!!!
 
-		# for each sentence in test data
-		for sentence in self.input[train_data_size:-2]:
+		for sentence in self.input[train_data_size:-2]: # for each sentence in test data
 		#for sentence in self.input[:train_data_size]:
 			for word in sentence:
 				print word[LEX], word[ID], '!', word[PARENT]
@@ -602,7 +658,144 @@ class MaltParser:
 					self.add_history_feature_map_for_training()
 					self.build_train_sparse_matrix()
 					# +1 for starting with 1, not 0
-					next_action = np.argmax(probs(np.array([self.train_samples.todense()]))) + 1
+					next_action = np.argmax(probs(np.array(self.train_samples).reshape([1,1,500]))) + 1
+					#print next_action #DEBUG
+
+					if len(self.stack) == 0:
+						self.shift()
+						break
+
+					word = self.input[self.current_sentence_position][self.current_word_position]
+					if next_action == LEFTARC:
+						if word[ID] == self.stack[-1][PARENT]:
+							#print '----------------------yes'
+							score += 1
+						self.left_arc()
+						continue
+
+					elif next_action == RIGHTARC:
+						#print 'exp right'
+						if word[PARENT] == self.stack[-1][ID]:
+							#print '----------------------yes'
+							score += 1
+						self.right_arc()
+						break
+
+					elif next_action == REDUCE:
+						self.reduce()
+						#print 'exp reduce'
+						continue
+
+					elif next_action == SHIFT:
+						self.shift()
+						#print 'exp shift'
+						break
+
+			self.current_sentence_position += 1
+
+			# test and count the score
+			#print 'sent ', self.current_sentence_position, 'score', score
+			general_score += ( score * 1.0 / (stop_idx - 1) )
+		print 'General score ', general_score
+		print '!!! Accuracy is ', general_score * 1.0 / len(self.input[train_data_size:-2])
+
+
+	def execute_simple_network_experiment(self, train_part):
+		print 'experiment starting...'
+		train_data_size = int(len(self.input) * train_part) # number of _sentences_
+		self.build_train_samples(0, train_data_size)
+		print "number of train samples: ", len(self.train_samples)
+
+		# work with network
+		print("Building network ...")
+   		#help(lasagne.layers.LSTMLayer)
+
+		# ===================================================
+		# Declarate the input/output format for network
+		# ===================================================
+   		#input_var = theano.sparse.csr_matrix(name='inputs', dtype='int16')
+	 	input_var = T.tensor3('inputs')
+	 	target_var = T.matrix('targets')
+	 	train_matrix = self.train_samples
+	 	print len(train_matrix), 'SHAPEEE'
+
+
+		# ===================================================
+		# Declarate the network architecture, layers.
+		# ===================================================
+	 	# (batch size, SEQ_LENGTH, num_features)
+	 	l_in = lasagne.layers.InputLayer(shape=(1, 1, 500), input_var=input_var)
+	 	l_in_drop = lasagne.layers.DropoutLayer(l_in, p=0.3)
+
+	 	# The l_forward layer creates an output of dimension (batch_size, SEQ_LENGTH, N_HIDDEN)
+	 	# Since we are only interested in the final prediction, we isolate that quantity and feed it to the next layer.
+	 	# The output of the sliced layer will then be of size (batch_size, N_HIDDEN)
+	 	#l_forward_slice = lasagne.layers.SliceLayer(l_forward_1, -1, 1)
+
+	 	l_out = lasagne.layers.DenseLayer(l_in_drop, num_units=10, W = lasagne.init.Normal(), nonlinearity=lasagne.nonlinearities.softmax)
+		l_out1 = lasagne.layers.DenseLayer(l_out, num_units=4, nonlinearity=lasagne.nonlinearities.softmax)
+
+	 	# lasagne.layers.get_output produces a variable for the output of the net
+	 	network_output = lasagne.layers.get_output(l_out1)
+	 	cost = T.nnet.categorical_crossentropy(network_output,target_var).mean()
+
+	 	# Retrieve all parameters from the network
+	 	all_params = lasagne.layers.get_all_params(l_out1,trainable=True)
+
+	 	# Compute AdaGrad updates for training
+	 	print("Computing updates ...")
+	 	updates = lasagne.updates.adagrad(cost, all_params, LEARNING_RATE)
+
+
+		# ===================================================
+		# Compiling the network functions for training&testing, computing cost.
+		# ===================================================
+	 	print("Compiling functions ...")
+	 	train = theano.function([input_var, target_var], cost, updates=updates, allow_input_downcast=True)
+	 	compute_cost = theano.function([input_var, target_var], cost, allow_input_downcast=True)
+
+	 	# Produce the probability distribution of the prediction
+	 	probs = theano.function([input_var],network_output,allow_input_downcast=True)
+
+
+		print "Training ..."
+		_startTime0 = time.time()
+	 	for idx, row in enumerate(train_matrix):
+			_startTime1 = time.time()
+			if idx % 100 == 0:
+				print "training", idx#, "!!!!!!time", (_startTime1 - _startTime0) / 100.0
+			inputs = np.array([row]).reshape([1,1,500])
+			targets = self.construct_targets_for_network(self.train_answers[idx])
+			_startTime2 = time.time()
+	 		avg_cost = train(inputs, targets)
+			_startTime3 = time.time()
+			print "sum time", _startTime3 - _startTime1, "reading", _startTime2 - _startTime1
+
+		print "Testing..."
+		# For each sentence predict consequence of actions, parse it using this actions and evaluate result
+		general_score = 0
+		self.current_sentence_position = train_data_size #!!!!!!!!!
+
+		# for each sentence in test data
+		for sentence in self.input[train_data_size:-2]:
+			for word in sentence:
+				print word[LEX], word[ID], '!', word[PARENT]
+
+			self.erase_state()
+			score = 0 # count matches in the sentence
+			stop_idx = len(sentence)
+
+			# for each word in the sentence
+			for idx, word_with_features in enumerate(sentence):
+				while self.current_word_position < stop_idx:
+					time1 = time.time()
+					self.erase_containers()
+					self.add_history_feature_map_for_training()
+					self.build_train_sparse_matrix()
+					# +1 for starting with 1, not 0
+					next_action = np.argmax(probs(np.array(self.train_samples).reshape([1,1,500]))) + 1
+					time2 = time.time()
+					print "prediction time is", time2 - time1
 					#print next_action #DEBUG
 
 					if len(self.stack) == 0:
@@ -652,5 +845,8 @@ if __name__ == "__main__":
 	mparser = MaltParser()
 	''' You can change the part to be training data	'''
 	train_part = 1.0 / 5 * 4
+
+	# By the stage self.train_data is filled, self.important_features is revealed.
+	mparser.select_features()
 	# mparser.execute_svm_experiment(train_part)
-	mparser.execute_network_experiment(train_part) # some parameters for learning&testing?
+	mparser.execute_network_experiment(train_part)
